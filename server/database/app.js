6d5e9fd709bebbc40de9aca179d142be8063f944
Barbosats <thiago.gui@hotmail.com>
2025-12-01 20:44:14 -0500

    Implementação dos endpoints da API

--- server/database/app.js
+++ server/database/app.js
@@ -59,16 +59,34 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
 //Write your code here
+try {
+    const documents = await Dealerships.find();
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+try {
+    const documents = await Dealerships.find({state: req.params.state});
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+try {
+    const document = await Dealerships.findOne({id: parseInt(req.params.id)});
+    res.json(document);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching document' });
+  }
 });
 
 //Express route to insert review
@@ -97,6 +115,36 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
     res.status(500).json({ error: 'Error inserting review' });
   }
 });
+
+// Express route to fetch all dealerships
+app.get('/fetchDealers', async (req, res) => {
+    try {
+      const documents = await Dealerships.find();
+      res.json(documents);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching documents' });
+    }
+  });
+
+  // Express route to fetch Dealers by a particular state
+app.get('/fetchDealers/:state', async (req, res) => {
+    try {
+      const documents = await Dealerships.find({ state: req.params.state });
+      res.json(documents);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching documents' });
+    }
+  });
+
+  // Express route to fetch dealer by a particular id
+app.get('/fetchDealer/:id', async (req, res) => {
+    try {
+      const document = await Dealerships.findOne({ id: parseInt(req.params.id) });
+      res.json(document);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching document' });
+    }
+  });
 
 // Start the Express server
 app.listen(port, () => {
