0743d04ba0c81d2ab6364edbe85ec4afd1740ff3
Barbosats <thiago.gui@hotmail.com>
2025-12-21 16:30:09 -0500

    car model, car make

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,26 +1,18 @@
 # Uncomment the required imports before adding the code
 
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
-from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
-
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
-from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout as django_logout
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
+from .models import CarMake, CarModel
 
-
-# Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-
-# Create your views here.
+# Login view
 @csrf_exempt
 def login_user(request):
     data = json.loads(request.body)
@@ -32,61 +24,43 @@ def login_user(request):
         return JsonResponse({"userName": username, "status": "Authenticated"})
     return JsonResponse({"status": "unauthorized"}, status=401)
 
-# Create a `login_request` view to handle sign in request
+# Logout view
 @csrf_exempt
-def login_user(request):
-    # Get username and password from request.POST dictionary
-    data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
-    # Try to check if provide credential can be authenticated
-    user = authenticate(username=username, password=password)
-    data = {"userName": username}
-    if user is not None:
-        # If user is valid, call login method to login current user
-        login(request, user)
-        data = {"userName": username, "status": "Authenticated"}
-    return JsonResponse(data)
+def logout_request(request):
+    django_logout(request)  # encerra sessão
+    return JsonResponse({"userName": ""})
 
-# Create a `logout_request` view to handle sign out request
-@csrf_exempt
-def logout(request): # Terminate user session
-    data = {"userName":""} # Return empty username
-    return JsonResponse(data)
-
-# Create a `registration` view to handle sign up request
+# Registration view
 @csrf_exempt
 def registration(request):
-    context = {}
-
-    # Load JSON data from the request body
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
     first_name = data['firstName']
     last_name = data['lastName']
     email = data['email']
-    username_exist = False
-    email_exist = False
-    try:
-        # Check if user already exists
-        User.objects.get(username=username)
-        username_exist = True
-    except:
-        # If not, simply log this is a new user
-        logger.debug("{} is new user".format(username))
 
-    # If it is a new user
-    if not username_exist:
-        # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
-        # Login the user and redirect to list page
+    try:
+        User.objects.get(username=username)
+        return JsonResponse({"userName": username, "error": "Already Registered"})
+    except User.DoesNotExist:
+        user = User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            email=email
+        )
         login(request, user)
-        data = {"userName":username,"status":"Authenticated"}
-        return JsonResponse(data)
-    else :
-        data = {"userName":username,"error":"Already Registered"}
-        return JsonResponse(data)
+        return JsonResponse({"userName": username, "status": "Authenticated"})
+
+# Get cars view
+def get_cars(request):
+    if CarMake.objects.count() == 0:
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = [{"CarModel": cm.name, "CarMake": cm.car_make.name} for cm in car_models]
+    return JsonResponse({"CarModels": cars})
 
 
 # # Update the `get_dealerships` view to render the index page with
